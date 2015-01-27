2c677d081f8e6aeef59654788532275105e97e19
mchaperc <mchaperc@yahoo.com>
2015-01-27 15:28:07 -0500

    Added Functionality

--- js/form-logic.js
+++ js/form-logic.js
@@ -2,14 +2,25 @@
 
 var list = [];
 
+//Autofocus on item
+
+$(document).ready(function() {
+	$('#item').focus();
+})
+
 //Validation of Item Names
 
 function formValid() {
-	var itemName = $('#item').val(), message = "Please enter an item";
+	var itemName = $('#item').val();
 	if (itemName === "" || itemName.toLowerCase() == "item name...") {
-		alert(message);
+		$('#required').show();
+		$('#item').focus();
 	} else {
-		alert(itemName);
+		if( $('#item').css('display') != 'none' ) {
+			$('#required').hide();
+		}
+		itemObj();
+		formClear();
 	}
 }
 
@@ -22,10 +33,34 @@ $(document).keypress(function(e) {
 	}
 });
 
+//Clear form fields after click or enter
+
+function formClear() {
+	$('#item').val('');
+	$('#qty').val('1');
+	$('#price').val('0');
+	$('#item').focus();
+}
+
 //Validation of Click to Add
 
-$(document).on('click', function(e) {
+$('.fa').on('click', function(e) {
 	formValid();
 });
 
-//Assigning item to List
+// //Creating an Item Object
+
+function itemObj() {
+	this.name = $('#item').val();
+	this.price = $('#price').val();
+	this.quantity = $('#qty').val();
+	list.push(this);
+	addItem();
+}
+
+//Adding Item to List
+	
+function addItem() {
+	$('.list-items').append('<p>(' + list[(list.length - 1)].quantity + ') ' + list[(list.length - 1)].name + '</p>');
+	$('.list-items').children().addClass('listing');
+}
