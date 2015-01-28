768d65574ba6a7ec20fa8389fc6c7c380561992d
mchaperc <mchaperc@yahoo.com>
2015-01-28 09:24:38 -0500

    added price total functionality and fixed list adding

--- js/form-logic.js
+++ js/form-logic.js
@@ -6,6 +6,7 @@ var list = [];
 
 $(document).ready(function() {
 	$('#item').focus();
+	itemCheck();
 })
 
 //Validation of Item Names
@@ -63,4 +64,25 @@ function itemObj() {
 function addItem() {
 	$('.list-items').append('<p>(' + list[(list.length - 1)].quantity + ') ' + list[(list.length - 1)].name + '</p>');
 	$('.list-items').children().addClass('listing');
+	//itemTotals();
+}
+
+//Totaling the list items' price
+
+function itemTotals() {
+	var total = 0;
+	for (var i = 0; i < list.length; i++) {
+		total += (list[i].price * list[i].quantity);
+	}
+	total = total * 1.06;
+	total = total.toFixed(2);
+	$('.actual-total').text('$' + total);
+}
+
+//Check Items when they're purchased
+
+function itemCheck() {
+	$('.listing').on('click', function() {
+		$(this).toggleClass('highlight');
+	});
 }
