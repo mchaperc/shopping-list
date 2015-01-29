5375265ace53a0774e7a354a7b25e9004121f927
mchaperc <mchaperc@yahoo.com>
2015-01-29 11:56:10 -0500

    Positioning and color changes

--- js/form-logic.js
+++ js/form-logic.js
@@ -64,25 +64,25 @@ function itemObj() {
 function addItem() {
 	$('.list-items').append('<p>(' + list[(list.length - 1)].quantity + ') ' + list[(list.length - 1)].name + '</p>');
 	$('.list-items').children().addClass('listing');
-	itemTotals();
+	// itemTotals();
 }
 
 //Totaling the list items' price
 
-function itemTotals() {
-	var total = 0;
-	for (var i = 0; i < list.length; i++) {
-		total += (list[i].price * list[i].quantity);
-	}
-	total = total * 1.06;
-	total = total.toFixed(2);
-	$('.actual-total').text('$' + total);
-}
+// function itemTotals() {
+// 	var total = 0;
+// 	for (var i = 0; i < list.length; i++) {
+// 		total += (list[i].price * list[i].quantity);
+// 	}
+// 	total = total * 1.06;
+// 	total = total.toFixed(2);
+// 	$('.actual-total').text('$' + total);
+// }
 
 //Check Items when they're purchased
 
 function itemCheck() {
-	$('.listing').on('click', function() {
+	$('.list-items').children().on('click', function() {
 		$(this).toggleClass('highlight');
 	});
 }
